3b84cda47f24430821f976113cc81e02d4c6ae83
reloadoskar <reloadoskar@gmail.com>
2023-12-01 10:09:24 -0600

    se agregan arrays de ejemplo para no sobrecargar la bdlocal con las modificaciones

---
+++ src/utils/arrayUbicacions.js
@@ -0,0 +1,64 @@
+const ubicacions = [
+    {
+        "_id": "651cdac6478725cf6f6c8852",
+        "nombre": "SUCURSAL 1",
+        "tipo": "SUCURSAL",
+        "direccion": "AV ALMOLOYA 56",
+        "telefono": "5589788451",
+        "__v": 0,
+        "capacidad": 500,
+        "email": "baltag@mail.com",
+        "empleados": [],
+        "updatedAt": "2023-12-01T01:59:54.087Z",
+        "horaf": "17:00",
+        "horai": "09:00",
+        "empaqueCapacidad": "CAJAS"
+    },
+    {
+        "_id": "651cdb0f478725cf6f6c8859",
+        "nombre": "BODEGA",
+        "tipo": "BODEGA",
+        "direccion": "CALLEJON 345, LAS ARMAS MEXICO",
+        "telefono": "5589949494",
+        "__v": 0,
+        "capacidad": 200,
+        "email": "cmarlo@mail.com",
+        "empleados": [],
+        "updatedAt": "2023-12-01T01:59:15.093Z",
+        "empaqueCapacidad": "CAJAS",
+        "horaf": "17:00",
+        "horai": "09:00"
+    },
+    {
+        "_id": "651cdb89478725cf6f6c8860",
+        "nombre": "SUCURSAL 2",
+        "tipo": "SUCURSAL",
+        "direccion": "AV CANO 45, COL BERGEL LA PAZ MEX",
+        "telefono": "89845454844",
+        "__v": 0,
+        "capacidad": 150,
+        "email": "gloribd@mail.com",
+        "empleados": [],
+        "updatedAt": "2023-12-01T02:00:12.420Z",
+        "empaqueCapacidad": "CAJAS",
+        "horaf": "17:00",
+        "horai": "09:00"
+    },
+    {
+        "_id": "65662820be67c22471a41640",
+        "nombre": "CUMBRES 34",
+        "tipo": "SUCURSAL",
+        "direccion": "CUMBRES 34, VILLA DE LAS FLORES",
+        "telefono": "67 12458963",
+        "__v": 0,
+        "capacidad": 200,
+        "email": "baltag@mail.com",
+        "empleados": [],
+        "updatedAt": "2023-12-01T02:00:33.164Z",
+        "empaqueCapacidad": "CAJAS",
+        "horaf": "18:00",
+        "horai": "10:00"
+    }
+]
+
+export default ubicacions
